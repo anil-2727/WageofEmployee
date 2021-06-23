bdd7708146cd3b8986482c1dde49a79024f67761
anil-2727 <bali39959@gmail.com>
2021-06-24 00:16:29 +0530

    conduction_hours_days

--- EmpWageUC6.java
+++ EmpWageUC6.java
@@ -1,20 +1,25 @@
 package javaworld;
 
-public class EmpWageUC5 {
+public class EmpWageUC6 {
+	
 
 	 public static final int Is_Full_Time = 1;
 	 public static final int Is_Part_Time = 2;
 	 public static final int Wage_Per_Hour = 20;
 	 public static final int Days_in_Month = 20;
-	
+	 public static final int Max_Hrs_In_Month = 100;
+	 
 	public static void main(String[] args){ 
 		 int emphr = 0;
 		 int empwage = 0;
 		 int total_employee_wage = 0;
-		
-		 for (int day = 0; day< Days_in_Month; day++){
-   int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-     System.out.println("empCheck="+empCheck);  
+		 int totalemphrs = 0;
+		 int totalWorkingDays = 0;
+
+	while(totalemphrs <= Max_Hrs_In_Month && totalWorkingDays < Days_in_Month){	 
+		totalWorkingDays++;
+  int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+    System.out.println("empCheck="+empCheck);  
 
 switch (empCheck){
 case 1:
@@ -29,11 +34,11 @@ default:
 	  System.out.println("Employee is Absent");
 	  emphr = 0;
 }
-empwage = ((Wage_Per_Hour)*(emphr));
-total_employee_wage += empwage;
-System.out.println("Daily Wage of Employee="+empwage);
+totalemphrs +=emphr ;
+System.out.println("day#: " + totalWorkingDays + "Emp Hr : " +emphr);
 		 }
-System.out.println("total Wage of Employee="+total_employee_wage);
+	int totalempwage = totalemphrs * Wage_Per_Hour;
+System.out.println("total Wage of Employee="+totalempwage);
 
 }
 }
