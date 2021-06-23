f7f1d5c820eddfb155027a52b68f896403cf3a36
anil-2727 <bali39959@gmail.com>
2021-06-24 00:32:40 +0530

    conduction

--- EmpWageUC6.java
+++ EmpWageUC6.java
@@ -1,45 +1,54 @@
 package javaworld;
 
 public class EmpWageUC6 {
-	
 
 	 public static final int Is_Full_Time = 1;
 	 public static final int Is_Part_Time = 2;
-	 public static final int Wage_Per_Hour = 20;
+	 public static final int wagePerHr = 20;
 	 public static final int Days_in_Month = 20;
-	 public static final int Max_Hrs_In_Month = 100;
-	 
-	public static void main(String[] args){ 
-		 int emphr = 0;
-		 int empwage = 0;
-		 int total_employee_wage = 0;
-		 int totalemphrs = 0;
-		 int totalWorkingDays = 0;
+	
+	public static void main(String[] args) {
 
-	while(totalemphrs <= Max_Hrs_In_Month && totalWorkingDays < Days_in_Month){	 
-		totalWorkingDays++;
-  int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-    System.out.println("empCheck="+empCheck);  
 
-switch (empCheck){
-case 1:
-	     System.out.println("Employee is FullTime Present");
-	    emphr = 8;
-	     break;
-case 2:
-	  System.out.println("Employee is PartTime Present");
-	  emphr = 4;
-	  break;
-default:
-	  System.out.println("Employee is Absent");
-	  emphr = 0;
+		int fullDayHr=8;
+		int partTimeHr=4;
+		int day=0;
+		int empHr=0;
+		int wage = 0;
+
+		while (day <= 19) {
+
+		int caseCheck = (int) (Math.random() * 10) % 3;
+
+		switch (caseCheck) {
+
+		case 1:
+				System.out.println("Present fullday");
+				wage = (wage + (wagePerHr * fullDayHr));
+            empHr = (empHr + fullDayHr);
+			 day++;
+			break;
+
+		case 2:
+				System.out.println("Present PartTime");
+			   wage = (wage + (wagePerHr * partTimeHr));
+			   empHr = (empHr + partTimeHr);
+			 day++;
+			break;
+
+		default:
+				System.out.println("Absent");
+		    day++;
+
+			if( empHr == 100)
+				break;
+		}
+			 }
+	
+		System.out.println("Total wage of a month is");
+		System.out.println(wage);
+		System.out.println(empHr);
+		System.out.println(day);
+
+	}
 }
-totalemphrs +=emphr ;
-System.out.println("day#: " + totalWorkingDays + "Emp Hr : " +emphr);
-		 }
-	int totalempwage = totalemphrs * Wage_Per_Hour;
-System.out.println("total Wage of Employee="+totalempwage);
-
-}
-}
-
